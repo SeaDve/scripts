d2d0a76ab96fe33aa9e41d2540b21185fcb2800a
SeaDve <davecruz48@gmail.com>
2021-08-16 09:56:36 +0800

    Use argparser

--- gettext-rs.py
+++ gettext-rs.py
@@ -1,41 +1,39 @@
 #!/usr/bin/env python3
 
-import os
-import sys
+import argparse
 import subprocess
+import sys
 
 BOLD = "\033[1m"
 BLUE = '\033[34m'
 ENDC = '\033[0m'
 
+
 def print_colored(header, text):
     print(f"{BOLD}{BLUE}{header}{ENDC}: {text}")
+
 
 def log(text):
     print_colored("INFO", text)
 
-if len(sys.argv) != 4:
-    print_colored("USAGE", "gettext-rs [meson-project-name] [src-dir] [build-dir]")
-    sys.exit()
-    
-app_name = sys.argv[1]
-src_dir = os.path.abspath(sys.argv[2])
-build_dir = os.path.abspath(sys.argv[3])
 
-try:
-    if input("Commit or stash unsaved changes before proceeding. Proceed? [y/N]") not in ("y", "Y"):
-        sys.exit()
-except KeyboardInterrupt:
+parser = argparse.ArgumentParser()
+parser.add_argument('project_name', help="The meson project name", type=str)
+parser.add_argument('src_dir', help="The source directory", type=str)
+parser.add_argument('build_dir', help="The building directory", type=str)
+args = parser.parse_args()
+
+if input("Commit or stash unsaved changes before proceeding. Proceed? [y/N]") not in ("y", "Y"):
     sys.exit()
 
 log("Replacing 'gettext!' with 'gettext'...")
-subprocess.run(f"find {src_dir} -type f -exec sed -i 's/gettext!/gettext/g' {{}} \;", shell=True, check=True)
+subprocess.run(f"find {args.src_dir} -type f -exec sed -i 's/gettext!/gettext/g' {{}} \;", shell=True, check=True)
 
-log(f"Running ninja -C {build_dir} {app_name}-pot...")
+log(f"Running ninja -C {args.build_dir} {args.project_name}-pot...")
 try:
-    subprocess.run(f"ninja -C {build_dir} {app_name}-pot", shell=True, check=True)
+    subprocess.run(f"ninja -C {args.build_dir} {args.project_name}-pot", shell=True, check=True)
 except subprocess.CalledProcessError:
     pass
 finally:
     log("Restoring directory...")
-    subprocess.run(f"git restore {src_dir}", shell=True, check=True)
+    subprocess.run(f"git restore {args.src_dir}", shell=True, check=True)
