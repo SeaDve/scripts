260c72088e0f66ab3ae9f05c844e451893536134
SeaDve <davecruz48@gmail.com>
2021-08-19 16:06:21 +0800

    Add working release-notes

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Hack to generate pot files for rust files with gettext macros. For some reason,
 normal ninja pot generator doesn't detect rust gettext macros (i.e. gettext!) 
 even when added as a keyword. This temporarily removes the `!`, generate the pot
 file, and restore the previous state.
+
+### release-notes
+
+```shell
+python release-notes.py [-h] appstream_file
+```
+
+Generate a release note based on the latest release in the appstream file.
--- release-notes.py
+++ release-notes.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+
+import argparse
+from xml.etree import ElementTree
+
+from utils import log
+
+parser = argparse.ArgumentParser()
+parser.add_argument('appstream_file', help="The path to the appstream file", type=str)
+args = parser.parse_args()
+
+tree = ElementTree.parse(args.appstream_file)
+root = tree.getroot()
+
+releases = root.find('releases')
+latest_release = releases[0]
+
+latest_version = latest_release.get('version')
+latest_release_description = latest_release.find('description')
+
+header = latest_release_description.find('p')
+body = latest_release_description.find('ul')
+
+log(f"Printing release notes for version {latest_version}...")
+
+print(header.text)
+for line in body:
+    print(f" * {line.text}")
