78a25a382a7cf03f7cf2918358393dd19c5cd742
SeaDve <davecruz48@gmail.com>
2021-08-20 10:32:32 +0800

    Make ovject oriented

--- release-notes.py
+++ release-notes.py
@@ -1,121 +1,104 @@
 #!/usr/bin/env python3
 
-import re
 import os
-import glob
-import fileinput
 import subprocess
 from xml.etree import ElementTree
 
-from utils import log
+import utils
+from utils import log, multiline_input
 
 
-def multiline_input()  -> str:
-    raise NotImplemented
+class Project:
+
+    def __init__(self, directory: str):
+        self.directory = directory
+        self.files = os.listdir(self.directory)
+
+        self.metainfo_file = self._get_metainfo_file()
+        self.meson_build_file = self._get_meson_build_file()
+        self.cargo_toml_file = self._get_cargo_toml_file()
+
+    def _get_metainfo_file(self) -> str:
+        data_files = os.listdir(os.path.join(self.directory, 'data'))
+        for file in data_files:
+            if 'metainfo' in file or 'appdata' in file:
+                return os.path.join(self.directory, 'data', file)
+
+    def _get_meson_build_file(self) -> str:
+        if 'meson.build' in self.files:
+            return os.path.join(self.directory, 'meson.build')
+        return None
+
+    def _get_cargo_toml_file(self) -> str:
+        if 'Cargo.toml' in self.files:
+            return os.path.join(self.directory, 'Cargo.toml')
+        return None
+
+    def _update_meson_version(self):
+        if self.meson_build_file is not None:
+            log("Meson build file found")
+            log(f"Replacing meson build version with new_version...")
+
+            utils.replace_in_file(r"version:\s*'(.*)'", f"version: '{self.new_version}'", self.meson_build_file)
+            log("Successfully replaced meson build's version with new version")
+        else:
+            log("Meson build file not found")
+            log("Skipping meson version update...")
+
+    def _update_cargo_version(self):
+        if self.cargo_toml_file is not None:
+            log("Cargo toml file found")
+            log(f"Replacing cargo toml version with new_version...")
+
+            utils.replace_in_file(r'version\s*=\s*"(.*)"', f'version = "{self.new_version}"', self.cargo_toml_file)
+            log("Successfully replaced cargo toml's version with new version")
+        else:
+            log("Cargo toml file not found")
+            log("Skipping cargo version update...")
+
+    def _update_metainfo_release_notes(self, release_notes: str):
+        raise NotImplementedError
+
+    def set_new_version(self, new_version: str):
+        self.new_version = new_version
+        self._update_cargo_version()
+        self._update_meson_version()
+
+    def commit_and_push_changes(self):
+        if input("Do you want to commit the changes? [y/N]") not in ("y", "Y"):
+            return
+
+        if self.metainfo_file is not None:
+            subprocess.run(['git', 'add', self.metainfo_file], check=True)
+            log("Added meson build to staged files")
+
+        if self.meson_build_file is not None:
+            subprocess.run(['git', 'add', self.meson_build_file], check=True)
+            log("Added cargo toml to staged files")
+
+        if self.cargo_toml_file is not None:
+            subprocess.run(['git', 'add', self.cargo_toml_file], check=True)
+            log("Added metainfo to staged files")
+
+        subprocess.run(['git', 'commit', '-m', f'chore: Bump to {self.new_version}'], check=True)
+        log("Changes committed")
+
+        if input("Do you want to push the changes? [y/N]") not in ("y", "Y"):
+            return
+
+        subprocess.run(['git', 'pull', 'origin', 'main'], check=True)
+        log("Pulled changes from origin/main")
+
+        subprocess.run(['git', 'push', 'origin', 'main'], check=True)
+        log("Pushed local changes to origin/main")
 
 
-def get_metainfo_file(project_directory) -> str:
-    data_files = os.listdir(os.path.join(project_directory, 'data'))
-    for file in data_files:
-        if 'metainfo' in file or 'appdata' in file:
-            return os.path.join(project_directory, 'data', file)
-    return None
-
-
-def edit_metainfo_file(metainfo_file, release_notes, new version):
-    raise NotImplemented
-
-
-def get_main_build_file(project_directory) -> str:
-    files = os.listdir(project_directory)
-    if 'meson.build' in files:
-        return os.path.join(project_directory, 'meson.build')
-    return None
-
-
-def get_cargo_file(project_directory) -> str:
-    files = os.listdir(project_directory)
-    if 'Cargo.toml' in files:
-        return os.path.join(project_directory, 'Cargo.toml')
-    return None
-
-
-def replace_with_new_version_meson(meson_build_file, new_version):
-    if meson_build_file is None:
-        log("Meson build file not found")
-        return
-
-    log("Meson build file found")
-    log(f"Replacing meson build version with new_version...")
-
-    with open(meson_build_file, 'r+') as file:
-        file_contents = file.read()
-        new_content = re.sub(r"version:\s*'(.*)'", f"version: '{new_version}'", file_contents, count=1)
-        file.seek(0)
-        file.truncate(0)
-        file.write(new_content)
-
-        log("Successfully replaced meson build's version with new version")
-
-
-def replace_with_new_version_cargo(cargo_toml_file, new_version):
-    if cargo_toml_file is None:
-        log("Cargo toml file not found")
-        return
-
-    log("Cargo toml file found")
-    log(f"Replacing cargo toml version with new_version...")
-
-    with open(cargo_toml_file, 'r+') as file:
-        file_contents = file.read()
-        new_content = re.sub(r'version\s*=\s*"(.*)"', f'version = "{new_version}"', file_contents, count=1)
-        file.seek(0)
-        file.truncate(0)
-        file.write(new_content)
-
-        log("Successfully replaced cargo toml's version with new version")
-
-
-def make_release_on_dir(project_directory, new_version):
-    metainfo_file = get_metainfo_file(project_directory)
-
+def make_release_on_dir(project_directory: str, new_version: str):
     log(f"Making release for version {new_version}...")
 
-    meson_build_file = get_main_build_file(project_directory)
-    cargo_toml_file = get_cargo_file(project_directory)
-
-    replace_with_new_version_meson(meson_build_file, new_version)
-    replace_with_new_version_cargo(cargo_toml_file, new_version)
-
-    return  # FIXME remove this
-
-    if input("Do you want to commit the changes? [y/N]") not in ("y", "Y"):
-        return
-
-    if metainfo_file is not None:
-        subprocess.call(['git', 'add', metainfo_file], check=True)
-        log("Added meson build to staged files")
-
-    if meson_build_file is not None:
-        subprocess.call(['git', 'add', meson_build_file], check=True)
-        log("Added cargo toml to staged files")
-
-    if cargo_toml_file is not None:
-        subprocess.call(['git', 'add', cargo_toml_file], check=True)
-        log("Added metainfo to staged files")
-
-    subprocess.call(['git', 'commit', '-m', f'chore: Bump to {new_version}'], check=True)
-    log("Changes committed")
-
-    if input("Do you want to push the changes? [y/N]") not in ("y", "Y"):
-        return
-
-    subprocess.call(['git', 'pull', 'origin', 'main'], check=True)
-    log("Pulled changes from origin/main")
-
-    subprocess.call(['git', 'push', 'origin', 'main'], check=True)
-    log("Pushed local changes to origin/main")
-
+    project = Project(project_directory)
+    project.set_new_version(new_version)
+    project.commit_and_push_changes()
 
 # tree = ElementTree.parse(args.appstream_file)
 # root = tree.getroot()
@@ -152,6 +135,7 @@ def make_release_on_dir(project_directory, new_version):
 
 #     print(output)
 
+
 if __name__ == '__main__':
     import argparse
     import sys
@@ -161,13 +145,13 @@ if __name__ == '__main__':
     parser.add_argument('version', help="The new version in format $N.$N.$N", type=str)
     args = parser.parse_args()
 
-    # FIXME uncommit this
+    # FIXME uncomment this
 
     # if input("Commit or stash unsaved changes before proceeding. Proceed? [y/N]") not in ("y", "Y"):
     #     sys.exit()
 
     make_release_on_dir(args.project_dir, args.version)
-    log("Make sure to update the pot files to")
+    log("Make sure to also update the pot files")
     log("Copy and paste this release note to github and make a release")
 
-    # FIXME paste rleease notes here
+    # FIXME paste release notes here
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import re
+
 BOLD = "\033[1m"
 BLUE = '\033[34m'
 ENDC = '\033[0m'
@@ -9,3 +11,16 @@ def print_colored(header, text):
 
 def log(text):
     print_colored("INFO", text)
+
+
+def replace_in_file(pattern: str, replacement: str, file_directory: str):
+    with open(file_directory, 'r+') as file:
+        file_contents = file.read()
+        new_content = re.sub(pattern, replacement, file_contents, count=1)
+        file.seek(0)
+        file.truncate(0)
+        file.write(new_content)
+
+
+def multiline_input() -> str:
+    raise NotImplementedError
