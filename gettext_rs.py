953d1c019264b14b3292aed88548b6d4695cd77a
SeaDve <davecruz48@gmail.com>
2021-08-21 13:09:32 +0800

    Use name == main

--- gettext_rs.py
+++ gettext_rs.py
@@ -1,31 +1,51 @@
 #!/usr/bin/env python3
 
-import argparse
 import subprocess
-import sys
 
 from utils import log, c_input
 
-parser = argparse.ArgumentParser()
-parser.add_argument('project_name', help="The meson project name", type=str)
-parser.add_argument('src_dir', help="The source directory", type=str)
-parser.add_argument('build_dir', help="The building directory", type=str)
-args = parser.parse_args()
 
-if c_input("Commit or stash unsaved changes before proceeding. Proceed? [y/N]") not in ("y", "Y"):
-    sys.exit()
+def replace_gettext_macros(src_dir: str) -> None:
+    log("Replacing 'gettext!' with 'gettext'...")
+    subprocess.run(
+        ["find", src_dir, "-type", "f", "-exec", "sed", "-i", "s/gettext!/gettext/g", "{}", ";"],
+        check=True
+    )
+    log("Successfully replaced 'gettext!' with 'gettext'")
 
-log("Replacing 'gettext!' with 'gettext'...")
-subprocess.run(
-    ["find", args.src_dir, "-type", "f", "-exec", "sed", "-i", "s/gettext!/gettext/g", "{}", ";"],
-    check=True
-)
 
-try:
+def generate_pot_files(build_dir: str, project_name: str) -> None:
     log("Generating pot file...")
-    subprocess.run(["ninja", "-C", args.build_dir, f"{args.project_name}-pot"], check=True)
-except subprocess.CalledProcessError:
-    pass
-finally:
-    log("Restoring directory...")
-    subprocess.run(["git", "restore", args.src_dir], check=True)
+    subprocess.run(["ninja", "-C", build_dir, f"{project_name}-pot"], check=True)
+    log("Pot file has been successfully generated")
+
+
+def restore_directory(src_dir: str) -> None:
+    log("Restoring src directory...")
+    subprocess.run(["git", "restore", src_dir], check=True)
+    log("The src directory has been restored to previous state")
+
+
+def main(project_name: str, src_dir: str, build_dir: str) -> None:
+    if c_input("Commit or stash unsaved changes before proceeding. Proceed? [y/N]") not in ("y", "Y"):
+        return
+
+    try:
+        replace_gettext_macros(src_dir)
+        generate_pot_files(build_dir, project_name)
+    except subprocess.CalledProcessError as error:
+        log(f"An error has occured: {error}")
+    finally:
+        restore_directory(src_dir)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('project_name', help="The meson project name", type=str)
+    parser.add_argument('src_dir', help="The source directory", type=str)
+    parser.add_argument('build_dir', help="The building directory", type=str)
+    args = parser.parse_args()
+
+    main(args.project_name, args.src_dir, args.build_dir)
